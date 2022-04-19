24664dccfac9339e053d9441881897047109366c
Keri Russin <karussin@student.fullsail.edu>
2022-04-18 22:32:42 -0400

    Practice lesson for Module 4 NodeJS API for MongoDB (Routing, CORS, and Parsing).

---
+++ README.md
@@ -0,0 +1,21 @@
+# NodeJS API for Mongodb
+## Routing, CORS, and Parsing
+### Keri Russin
+### WDV353 | Eric Clarke | Web Development | April 18, 2022
+
+# Assignment Example
+## Module 4
+
+----------------------------------------------
+
+npm init
+npm i
+    express
+    dotenv
+    morgan
+
+npm i -D
+    nodemon
+    jest
+
+npm start
---
+++ app/app.js
@@ -0,0 +1,61 @@
+const express = require("express");
+const app = express();
+const morgan = require("morgan");
+const authorRoutes = require("../api/routes/authors");
+const bookRoutes = require("../api/routes/books");
+
+
+// middleware for logging
+app.use(morgan("dev"));
+// parsing middleware
+app.use(express.urlencoded({
+    extended: true
+}));
+
+// middleware that all request are json
+app.use(express.json());
+
+//middleware to handle the CORS Policy
+app.use((req, res, next) => {
+    res.header("Access-Control_Allow_Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization ");
+
+    if(req.method === "OPTIONS"){
+        res.header("Access-Control-Allow-Methods", "POST, PUT, GET, PATCH, DELETE");
+    }
+    next();
+});
+
+app.get("/", (req, res, next) => {
+    res.status(201).json({
+        message: "Service is up!",
+        method: req.method
+    });
+});
+
+app.use("/authors", authorRoutes);
+app.use("/books", bookRoutes);
+
+
+
+
+// add middleware to handle errors and bad url paths
+app.use((req, res, next) => {
+    const error = new Error("NOT FOUND!");
+    error.status = 404;
+    next(error);
+});
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500).json({
+        error: {
+            message: error.message,
+            status: error.status
+        }
+    });
+});
+
+
+
+
+module.exports = app;
